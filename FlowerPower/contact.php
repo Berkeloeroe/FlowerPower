2a126c3af7c3c38fdd4d8f39c7bc947019032427
Randy <54804029+Berkeloeroe@users.noreply.github.com>
2021-01-26 11:46:02 +0100

    Add files via upload

---
+++ FlowerPower/contact.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="https://www.eatlogos.com/alphabet_logos/png/vector_alphabet_o_green_logo.png">
+    <title>FlowerPower</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <h3>FlowerPower</h3>
+  <img id="logo" src="https://www.eatlogos.com/alphabet_logos/png/vector_alphabet_o_green_logo.png" alt="picca">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="loginEmployee.php">Inloggen medewerker</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="loginCustomer.php">Inloggen klant</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="registreren.php">Registreren <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Zoeken" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken</button>
+    </form>
+  </div>
+</nav>
+
+<center>
+<h3>Contact</h3><br>
+<i class="fa fa-map-marker">&nbsp Amsterdam</i><br>
+<i class="fa fa-phone">&nbsp 0644863640</i><br>
+<i class="fa fa-envelope">&nbsp flowerpower@outlook.com</i>
+</center>
+
+</body>
+</html>
