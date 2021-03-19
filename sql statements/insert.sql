4f09c290435fadfd9032495f3e761b3656356096
Randy <54804029+Berkeloeroe@users.noreply.github.com>
2021-03-19 20:23:45 +0100

    Add files via upload

--- sql statements/insert.sql
+++ sql statements/insert.sql
@@ -2,3 +2,8 @@ INSERT INTO artikel (Artikelcode, Artikel, Prijs)
 VALUES (Null, 'Roos', 1.00),
 (Null, 'Tulp', 4.20),
 (Null, 'Zonnebloem', 2.00);
+
+INSERT INTO medewerker (Medewerkerscode, Voorletters, Voorvoegsels, Achternaam, Gebruikersnaam, Wachtwoord)
+VALUES (Null, 'Jan', Null, 'Pieter', 'JanP', 'jan123'),
+(Null, 'Mohammed', 'de', 'Jong', 'Mootje020', 'mootje123'),
+(Null, 'Ferdinand', 'el', 'Amrani', 'Ferdani', 'ferdi123');
